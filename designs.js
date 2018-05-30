2106021e9f343eb69119ccc49aeb356a9d7ed19d
Paola TesoritoLi <paola.perez.masedo@gmail.com>
2018-05-30 19:16:46 +0200

    added a new functionality for the instructions of the game with a click event and .toggleClass()

--- designs.js
+++ designs.js
@@ -1,5 +1,8 @@
-$(document).ready(function() {
+$(document).ready(function(){
 
+/*
+*Function to make the grid that will hold the colors
+*/
 
   function makeGrid() {
     let height = $("#inputHeight").val();
@@ -16,14 +19,31 @@ $(document).ready(function() {
       }
     }
   }
+
+//Submit button
   let form = $("#sizePicker");
   form.submit(function(event) {
     event.preventDefault();
     makeGrid();
   });
 
+/*
+*Function to print the colors
+*/
+
   $("table").on("click", "td", function() {
     let color = $("#colorPicker").val();
     $(this).css("background-color", color);
   });
+
+$(".btnHow").click(function(){
+  $("div.instructions").toggleClass("show_instructions");
+});
+
+
+$(".instructions_close").click(function(){
+  $("div.instructions").css("display", "none");
+});
+
+
 });
